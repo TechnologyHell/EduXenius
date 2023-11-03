f78bb92a912aead8467a194bc691da4579f7b35c
Nitish Singh <89546196+TechnologyHell@users.noreply.github.com>
2023-11-03 12:27:56 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
+from flask import Flask, render_template, request
 import pandas as pd
-from flask import Flask, request, jsonify
 
-app = Flask(__name)
+app = Flask(__name__)
 
-# Load the CSV data into a Pandas DataFrame
-df = pd.read_csv('course-db.csv')
+# Load course data from the CSV file
+course_data = pd.read_csv('courses.csv')
+
+@app.route('/')
+def index():
+    return render_template('recommend.html', courses=[])
 
 @app.route('/recommend', methods=['POST'])
-def recommend_courses():
-    keyword = request.form['keyword']  # Get the keyword from the form
 
-    # Filter courses with the keyword in the description
-    matching_courses = df[df['Course Description'].str.contains(keyword, case=False)]
-    top_3_courses = matching_courses.head(3)  # Get the top 3 matching courses
+def recommend():
+    keyword = request.form['interestInput']
 
-    recommendations = top_3_courses[['Course Name', 'Course URL']].to_dict(orient='records')
+    # Filter courses based on the keyword in the description
+    matching_courses = course_data[course_data['Course Description'].str.contains(keyword, case=False)]
 
-    return jsonify(recommendations)
+    # Get the first 3 matching courses as recommendations
+    recommended_courses = matching_courses['Course Name'][:3].tolist()
+
+    return render_template('recommend.html', courses=recommended_courses)
+
+def filter_courses_by_keyword(keyword):
+    # Filter courses based on the keyword in the description
+    matching_courses = course_data[course_data['Course Description'].str.contains(keyword, case=False)]
+
+    # Get the first 3 matching courses as recommendations
+    recommended_courses = matching_courses['Course Name'][:3].tolist()
+
+    return recommended_courses
 
 if __name__ == '__main__':
     app.run(debug=True)
